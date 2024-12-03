a9170ebd11de16e224cebefefd4618410c7cc5a0
Morimel <melsovisa77@gmail.com>
2024-12-03 11:42:28 +0600

    Initial Commit

---
+++ ProjectCS25Tests/ProjectCS25Tests.swift
@@ -0,0 +1,17 @@
+//
+//  ProjectCS25Tests.swift
+//  ProjectCS25Tests
+//
+//  Created by Isa Melsov on 3/12/24.
+//
+
+import Testing
+@testable import ProjectCS25
+
+struct ProjectCS25Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
